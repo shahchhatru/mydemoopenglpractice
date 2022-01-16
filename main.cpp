c9797c711c3377db9770feafd2a2607d4d595c36
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-01-16 13:10:26 +0545

    using index vertices

--- main.cpp
+++ main.cpp
@@ -36,6 +36,14 @@ int main() {
 		0.0f,-0.7f,0.4f,
 	};
 
+	GLuint indices[] = {
+		0,3,5, //lower left triangle
+		3,2,9,
+		5,4,1
+	};
+
+
+
 
 	//datatype for window object
 	GLFWwindow* window = glfwCreateWindow(800,800,"Grph polty",NULL,NULL);
@@ -71,18 +79,22 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 	
-	GLuint VAO,VBO;
+	GLuint VAO,VBO,EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 	//
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices,GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,3*sizeof(float),(void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER,0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glClearColor(0.7f, 0.13f, 0.17f,1.0f); //rgba
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
@@ -94,14 +106,15 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 8);
-		
+		//glDrawArrays(GL_TRIANGLE_FAN, 0, 8);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
+	
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	//once we are done with window we want to delete it

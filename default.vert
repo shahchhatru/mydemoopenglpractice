ee20c133e7ccf73addb682367c9ff7f391a00e99
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-01-20 10:10:40 +0545

    rotating pyramid X,YandZ axis

--- default.vert
+++ default.vert
@@ -11,20 +11,18 @@ layout (location = 2) in vec2 aTex;
 // Outputs the color for the Fragment Shader
 out vec3 color;
 // Outputs the texture coordinates to the fragment shader
-//out vec2 texCoord;
+out vec2 texCoord;
 
-// Controls the scale of the vertices
-uniform float scale;
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 proj;
+// Imports the camera matrix from the main function
+uniform mat4 camMatrix;
+
 
 void main()
 {
 	// Outputs the positions/coordinates of all vertices
-	gl_Position = proj*view*model*vec4(aPos,1.0);
+	gl_Position = camMatrix * vec4(aPos, 1.0);
 	// Assigns the colors from the Vertex Data to "color"
 	color = aColor;
 	// Assigns the texture coordinates from the Vertex Data to "texCoord"
-	//texCoord = aTex;
+	texCoord = aTex;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //------- Ignore this ----------
-#include<filesystem>
-namespace fs = std::filesystem;
+//#include<filesystem>
+//namespace fs = std::filesystem;
 //------------------------------
 
 #include<iostream>
@@ -16,7 +16,7 @@ namespace fs = std::filesystem;
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
-
+#include"Camera.h"
 
 const unsigned int width = 800;
 const unsigned int height = 800;
@@ -27,8 +27,8 @@ GLfloat vertices[] =
 { //     COORDINATES     /        COLORS      /   TexCoord  //
 	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	//0.0f, 0.0f,
 	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	//5.0f, 0.0f,
-	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	//0.0f, 0.0f,
-	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	//5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.90f, 0.00f, 0.44f,	//0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.00f, 0.70f, 0.44f,	//5.0f, 0.0f,
 	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	//2.5f, 5.0f
 };
 
@@ -77,10 +77,8 @@ int main()
 	glViewport(0, 0, width, height);
 
 
-
 	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
-
 
 
 	// Generates Vertex Array Object and binds it
@@ -94,15 +92,14 @@ int main()
 
 	// Links VBO attributes such as coordinates and colors to VAO
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT,  6* sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	//VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	// Gets ID of uniform called "scale"
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
 
 	/*
 	* I'm doing this relative path thing in order to centralize all the resources into one folder and not
@@ -121,12 +118,13 @@ int main()
 	/*Texture brickTex("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	brickTex.texUnit(shaderProgram, "tex0", 0);*/
 
-	// Variables that help the rotation of the pyramid
-	float rotation = 0.0f;
-	double prevTime = glfwGetTime();
+
 
 	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
+
+	// Creates camera object
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -138,34 +136,11 @@ int main()
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
 
-		// Simple timer
-		double crntTime = glfwGetTime();
-		if (crntTime - prevTime >= 1 / 60)
-		{
-			rotation += 0.05f;
-			prevTime = crntTime;
-		}
+		// Handles camera inputs
+		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
-		// Initializes matrices so they are not the null matrix
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 proj = glm::mat4(1.0f);
-
-		// Assigns different transformations to each matrix
-		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-		proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-
-		// Outputs the matrices into the Vertex Shader
-		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
-		glUniform1f(uniID, 0.5f);
 		// Binds texture so that is appears in rendering
 		//brickTex.Bind();
 		// Bind the VAO so OpenGL knows to use it

2a5707db8cab8c221d4ec7a612eb6ac146743072
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-02-28 09:26:13 +0545

    taking input from user commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 //------------------------------
 
 #include<iostream>
+#include<stdlib.h>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<stb/stb_image.h>
@@ -19,8 +20,8 @@
 #include"Camera.h"
 
 #include"Zmatrix.h";
-const unsigned int width = 800;
-const unsigned int height = 800;
+const unsigned int width = 1000;
+const unsigned int height = 1000;
 GLfloat vertices[600] ;
 GLuint indices[600] ;
 
@@ -49,136 +50,150 @@ GLuint indices[] =
 
 int main()
 {
-	string infix = "1-x/7-y/5+x^2";
-	string postfix = Parser::infixtoPostfix(infix);
-	Zmatrix z(10, 10,indices);
-	z.evaluate(postfix);
-	z.createVertexArray(vertices);
-	z.displayXYZ(vertices);
-	//vertices = z.vertices;
-	//indices = z.indices;
-	z.displayIndexArray(indices);
-	//cout << z.numindices << endl;
-	// Initialize GLFW
-	glfwInit();
+	//string infix = "x*y+e(x*x)/10-e(x*y)/10+e(y*y)/10";
+	//string infix = "x*y+e(x*x)/10-e(x*y)/10+e(y*y)/10 +cos(x*y)/10";
+	//string infix = "x/10-y+1/10";
+	//string infix = "1/10-x*x*sin(y*y)/5+y*cos(x*x*x)/10+x*y";
+	//string infix = "x*x/y+y*y/x";
+	string infix;
+	char flag='y';
+	while (flag == 'y') {
 
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		system("CLS");
+		std::cout << "Enter the infix expression" << std::endl;
+		std::cin >> infix;
+		string postfix = Parser::infixtoPostfix(infix);
+		Zmatrix z(10, 10, indices);
+		z.evaluate(postfix);
+		z.createVertexArray(vertices);
+		z.displayXYZ(vertices);
+		//vertices = z.vertices;
+		//indices = z.indices;
+		z.displayIndexArray(indices);
+		//cout << z.numindices << endl;
+		// Initialize GLFW
+		glfwInit();
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	// Introduce the window into the current context
-	glfwMakeContextCurrent(window);
+		// Tell GLFW what version of OpenGL we are using 
+		// In this case we are using OpenGL 3.3
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		// Tell GLFW we are using the CORE profile
+		// So that means we only have the modern functions
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//Load GLAD so it configures OpenGL
-	gladLoadGL();
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, width, height);
+		// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+		GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
+		// Error check if the window fails to create
+		if (window == NULL)
+		{
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return -1;
+		}
+		// Introduce the window into the current context
+		glfwMakeContextCurrent(window);
 
-
-	// Generates Shader object using shaders default.vert and default.frag
-	Shader shaderProgram("default.vert", "default.frag");
-
-
-	// Generates Vertex Array Object and binds it
-	VAO VAO1;
-	VAO1.Bind();
-
-	// Generates Vertex Buffer Object and links it to vertices
-	//VBO VBO1(vertices, sizeof(vertices));
-	VBO VBO1(vertices, sizeof(vertices));
-	cout <<"sizeof "<< sizeof(vertices) << endl << endl;
-	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(indices, sizeof(indices));
-
-	// Links VBO attributes such as coordinates and colors to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	//VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	// Unbind all to prevent accidentally modifying them
-	VAO1.Unbind();
-	VBO1.Unbind();
-	//EBO1.Unbind();
+		//Load GLAD so it configures OpenGL
+		gladLoadGL();
+		// Specify the viewport of OpenGL in the Window
+		// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+		glViewport(0, 0, width, height);
 
 
-
-	/*
-	* I'm doing this relative path thing in order to centralize all the resources into one folder and not
-	* duplicate them between tutorial folders. You can just copy paste the resources from the 'Resources'
-	* folder and then give a relative path from this folder to whatever resource you want to get to.
-	* Also note that this requires C++17, so go to Project Properties, C/C++, Language, and select C++17
-	*/
-	//std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
-	//std::string texPath = "/Resources/YoutubeOpenGL 7 - Going 3D/";
-
-	// Texture
-	//Texture brickTex((parentDir + texPath + "brick.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	//brickTex.texUnit(shaderProgram, "tex0", 0);
-
-	// Original code from the tutorial
-	/*Texture brickTex("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	brickTex.texUnit(shaderProgram, "tex0", 0);*/
+		// Generates Shader object using shaders default.vert and default.frag
+		Shader shaderProgram("default.vert", "default.frag");
 
 
-
-	// Enables the Depth Buffer
-	glEnable(GL_DEPTH_TEST);
-
-	// Creates camera object
-	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
-
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Specify the color of the background
-		glClearColor(0.00f, 0.00f, 0.00f, 1.0f);
-		// Clean the back buffer and depth buffer
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		shaderProgram.Activate();
-
-		// Handles camera inputs
-		camera.Inputs(window);
-		// Updates and exports the camera matrix to the Vertex Shader
-		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
-
-		// Binds texture so that is appears in rendering
-		//brickTex.Bind();
-		// Bind the VAO so OpenGL knows to use it
+		// Generates Vertex Array Object and binds it
+		VAO VAO1;
 		VAO1.Bind();
-		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES,z.numindices , GL_UNSIGNED_INT, 0);
-		//glDrawArrays(GL_TRIANGLES, 0,15);
-		// Swap the back buffer with the front buffer
-		glfwSwapBuffers(window);
-		// Take care of all GLFW events
-		glfwPollEvents();
+
+		// Generates Vertex Buffer Object and links it to vertices
+		//VBO VBO1(vertices, sizeof(vertices));
+		VBO VBO1(vertices, sizeof(vertices));
+		cout << "sizeof " << sizeof(vertices) << endl << endl;
+		// Generates Element Buffer Object and links it to indices
+		EBO EBO1(indices, sizeof(indices));
+
+		// Links VBO attributes such as coordinates and colors to VAO
+		VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+		VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		//VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		// Unbind all to prevent accidentally modifying them
+		VAO1.Unbind();
+		VBO1.Unbind();
+		//EBO1.Unbind();
+
+
+
+		/*
+		* I'm doing this relative path thing in order to centralize all the resources into one folder and not
+		* duplicate them between tutorial folders. You can just copy paste the resources from the 'Resources'
+		* folder and then give a relative path from this folder to whatever resource you want to get to.
+		* Also note that this requires C++17, so go to Project Properties, C/C++, Language, and select C++17
+		*/
+		//std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
+		//std::string texPath = "/Resources/YoutubeOpenGL 7 - Going 3D/";
+
+		// Texture
+		//Texture brickTex((parentDir + texPath + "brick.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+		//brickTex.texUnit(shaderProgram, "tex0", 0);
+
+		// Original code from the tutorial
+		/*Texture brickTex("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+		brickTex.texUnit(shaderProgram, "tex0", 0);*/
+
+
+
+		// Enables the Depth Buffer
+		glEnable(GL_DEPTH_TEST);
+
+		// Creates camera object
+		Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+		// Main while loop
+		while (!glfwWindowShouldClose(window))
+		{
+			// Specify the color of the background
+			glClearColor(0.00f, 0.00f, 0.00f, 1.0f);
+			// Clean the back buffer and depth buffer
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			// Tell OpenGL which Shader Program we want to use
+			shaderProgram.Activate();
+
+			// Handles camera inputs
+			camera.Inputs(window);
+			// Updates and exports the camera matrix to the Vertex Shader
+			camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+
+			// Binds texture so that is appears in rendering
+			//brickTex.Bind();
+			// Bind the VAO so OpenGL knows to use it
+			VAO1.Bind();
+			// Draw primitives, number of indices, datatype of indices, index of indices
+			glDrawElements(GL_TRIANGLES, z.numindices, GL_UNSIGNED_INT, 0);
+			//glDrawArrays(GL_TRIANGLES, 0,15);
+			// Swap the back buffer with the front buffer
+			glfwSwapBuffers(window);
+			// Take care of all GLFW events
+			glfwPollEvents();
+		}
+
+
+
+		// Delete all the objects we've created
+		VAO1.Delete();
+		VBO1.Delete();
+		EBO1.Delete();
+		//brickTex.Delete();
+		shaderProgram.Delete();
+		// Delete window before ending the program
+		glfwDestroyWindow(window);
+		// Terminate GLFW before ending the program
+		glfwTerminate();
+		std::cout << "do you want to enter more eqn (y/n)" << std::endl;
+		std::cin >> flag;
 	}
-
-
-
-	// Delete all the objects we've created
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
-	//brickTex.Delete();
-	shaderProgram.Delete();
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
 	return 0;
 }
